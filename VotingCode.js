7c738d78ec86212f8bba775fc6655284628e8cab
Mink-Mink <alekseyminkin96@gmail.com>
2020-05-14 17:34:04 +0200

    Main app functionality finished

--- VotingCode.js
+++ VotingCode.js
@@ -1,11 +1,19 @@
 'use strict';
 
+let sum = (total, current) => {return Number(total) + Number(current)};
+
+function voteTokensToVotes(voteTokens) {
+  return Math.floor(Math.sqrt(voteTokens));
+}
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { questionsToDecide: [{text: "", options: ["",""]}],
-                   voters: [],
-                   voteBudget: 100 };
+    this.state = { questionsToDecide: [{text: "Abra", options: ["Kadanbra","Alakazam"]}, {text: "Pichu", options: ["Pickachu","Raichu"]}],
+                   voterList: ["Voter 1", "Voter 2"],
+                   voteBudget: 100,
+                   voteState: -1 };
   }
 
   addVoteQuestion() {
@@ -14,11 +22,9 @@ class App extends React.Component {
   }
 
   removeVoteQuestion(questionNumber) {
-    if ((questionNumber >= 0) && (questionNumber < this.state.questionsToDecide.length)) {
-      var tempQuestions = this.state.questionsToDecide
-      tempQuestions.splice(questionNumber, 1 );
-      this.setState({questionsToDecide: tempQuestions});
-    }
+    let tempQuestions = this.state.questionsToDecide
+    tempQuestions.splice(questionNumber, 1 );
+    this.setState({questionsToDecide: tempQuestions});
   }
 
   addVoteOptionToQuestion(questionNumber) {
@@ -44,64 +50,142 @@ class App extends React.Component {
     this.setState({questionsToDecide: this.state.questionsToDecide});
   }
 
+  onAddVoter() {
+    this.state.voterList.push("");
+    this.setState({voterList: this.state.voterList});
+  }
+
+  onRemoveVoter(voterNumber) {
+    let tempVoters = this.state.voterList;
+    if (tempVoters.length >= 2) {
+      tempVoters.splice(voterNumber, 1 );
+      this.setState({voterList: tempVoters});
+    }
+  }
+
+  onChangeVoter(voterNumber, event) {
+    this.state.voterList[voterNumber] = event.target.value;
+    this.setState({voterList: this.state.voterList});
+  }
+
   onEditVoteTotal(event) {
     this.setState({voteBudget: event.target.value});
   }
 
-  submitVote() {
+  startVote() {
+    this.state.voterList = this.state.voterList.map((voter) => { 
+      return {name: voter, 
+              remainingVoteBudget: this.state.voteBudget,
+              votes: this.state.questionsToDecide.map((question) => {
+                let optionVotes = [];
+                optionVotes.length = question.options.length;
+                optionVotes.fill(0);
+                return optionVotes})
+      }});
+    this.setState({voteState: 0, voterList: this.state.voterList});
+  }
 
+  onChangeVote( voterNumber, questionNumber, optionNumber, event) {
+    let input = Number(event.target.value);
+    const targetVoter = this.state.voterList[voterNumber];
+    if ( input >= 0) {
+      targetVoter.votes[questionNumber][optionNumber] = input;
+    } else {
+      targetVoter.votes[questionNumber][optionNumber] = 0;
+    }
+    let tokensSpent = this.getTotalVoteTokensSpent(voterNumber);
+    targetVoter.remainingVoteBudget = this.state.voteBudget - tokensSpent;
+    this.setState({voterList: this.state.voterList});
+  }
+
+  getTotalVoteTokensSpent(voterNumber) {
+    return this.state.voterList[voterNumber].votes.reduce((total, current) => {return current.reduce(sum, 0) + total;} , 0);
+  }
+
+  giveBallotToNextVoter() {
+    if ( this.getTotalVoteTokensSpent(this.state.voteState) > this.state.voteBudget) {
+      window.alert("Too many tokens spent! Spend less.");
+    } else {
+      this.state.voteState++;
+      this.setState({voteState: this.state.voteState});
+    }
+  }
+
+  calculateTotalVotes() {
+    let voteTotals = [];
+    for (let questionNumber = 0; questionNumber < this.state.questionsToDecide.length; questionNumber++){
+      voteTotals.push([]);
+      for (let optionNumber = 0; optionNumber < this.state.questionsToDecide[questionNumber].options.length; optionNumber++) {
+        voteTotals[questionNumber].push(0);
+        for (const voter of this.state.voterList) {
+          voteTotals[questionNumber][optionNumber] += voteTokensToVotes(voter.votes[questionNumber][optionNumber]);
+        }
+      }
+    }
+    return voteTotals;
+  }
+
+  resetVote() {
+    this.state.voterList = this.state.voterList.map((voter) => {return voter.name}); 
+    this.setState({voteState: -1, voterList: this.state.voterList});
   }
 
   render() {
-    return (<div>
-      <QuestionList questions = {this.state.questionsToDecide} 
-                    onAddQuestion = {() => this.addVoteQuestion()}
-                    onRemoveQuestion = {(i) => this.removeVoteQuestion(i)}
-                    onAddOption = {(i) => this.addVoteOptionToQuestion(i)}
-                    onRemoveOption = {(i, j) => this.removeVoteOptionFromQuestion(i, j)}
-                    onChangeQuestion = {(i, e) => this.onChangeQuestion(i, e)}
-                    onChangeOption = {(i, j, e) => this.onChangeOption(i, j, e)}/>
-      <VoterList voters = {this.state.voters}/>
-      <VotingBudget voteBudget = {this.state.voteBudget} onEdit = {(e) => this.onEditVoteTotal(e)}/>
-    </div>); 
+    if (this.state.voteState === -1) {
+      return (<div>
+        <QuestionList questions = {this.state.questionsToDecide} 
+                      onAddQuestion = {() => {this.addVoteQuestion()}}
+                      onRemoveQuestion = {(i) => {this.removeVoteQuestion(i)}}
+                      onAddOption = {(i) => {this.addVoteOptionToQuestion(i)}}
+                      onRemoveOption = {(quest, opt) => {this.removeVoteOptionFromQuestion(quest, opt)}}
+                      onChangeQuestion = {(quest, e) => {this.onChangeQuestion(quest, e)}}
+                      onChangeOption = {(quest, opt, e) => {this.onChangeOption(quest, opt, e)}}/>
+        <VoterList voterList = {this.state.voterList}
+                  onAddVoter = {() => {this.onAddVoter()}}
+                  onRemoveVoter = {(voter) => {this.onRemoveVoter(voter)}}
+                  onChangeVoter = {(voter, e) => {this.onChangeVoter(voter, e)}}/>
+        <VotingBudget voteBudget = {this.state.voteBudget} onChange = {(e) => {this.onEditVoteTotal(e)}}/>
+        <button onClick={() => {this.startVote()}}>Start the vote</button>
+      </div>);
+    } else if (this.state.voteState < this.state.voterList.length) {
+      let currentVoter = this.state.voterList[this.state.voteState];
+      return (<div>
+        <p>Currently voting {currentVoter.name}. Vote budget left: {currentVoter.remainingVoteBudget}</p>
+        <Ballot questions = {this.state.questionsToDecide} 
+                onChangeVote = {(quest, opt, e) => {this.onChangeVote(this.state.voteState, quest, opt, e)}}
+                votes = {currentVoter.votes}/>
+        <button onClick={() => {this.giveBallotToNextVoter()}}>Submit the vote</button>
+      </div>);
+    } else {
+      const voteTotals = this.calculateTotalVotes();
+      return (<div>
+        <p>Vote concluded. Final vote counts:</p>
+        <VoteResults questions = {this.state.questionsToDecide} voteTotals = {voteTotals}/>
+        <button onClick={() => {this.resetVote()}}>Start over?</button>
+      </div>);
+    };
   }
 }
 
 class QuestionList extends React.Component {
   render() {
-    let votedQuestions = this.props.questions.map((question, questionNumber) => { return (
-        <li key={questionNumber}>
-          <TextForm formText = {question.text} 
-                    onChange = {(e) => this.props.onChangeQuestion(questionNumber, e)} 
-                    onRemove = {() => this.props.onRemoveQuestion(questionNumber)}/>
-          <ol>
-            {question.options.map((option, optionNumber) => {
-                return <li key = {optionNumber}>
-                  <TextForm formText = {option} 
-                            onChange = {(e) => this.props.onChangeOption(questionNumber, optionNumber, e)}
-                            onRemove = {() => this.props.onRemoveOption(questionNumber, optionNumber)}/></li>
-            })}
-          </ol>
-          <button onClick={() => {this.props.onAddOption(questionNumber)}}>Add another vote option</button>
-        </li>)});
-    return (<div>
-              <div>List of questions that will be voted on.</div>
-                <ol>
-                  {votedQuestions}
-                </ol>
-              <button onClick = {this.props.onAddQuestion}> Add another question </button>
-            </div>);
-  }
-}
-
-class TextForm extends React.Component {
-  render() {
+    let questionTexts = [];
+    let optionLists = [];
+    for (let questionNumber = 0; questionNumber < this.props.questions.length; questionNumber++) {
+      const currentQuestion = this.props.questions[questionNumber];
+      questionTexts.push(currentQuestion.text);
+      optionLists.push(<VariableLengthList contentList = {currentQuestion.options} 
+                                onChange = {(optionNumber, e) => {this.props.onChangeOption(questionNumber, optionNumber, e)}}
+                                onRemove = {(optionNumber) => {this.props.onRemoveOption(questionNumber, optionNumber)}}
+                                onAdd = {() => {this.props.onAddOption(questionNumber)}}/>);
+    }
     return ( <div>
-              {this.props.label ? this.props.label : null}
-              <label>
-                <input type="text" value={this.props.formText} onChange={this.props.onChange} />      
-                {this.props.onRemove ? <button onClick = {this.props.onRemove}>X</button> : null}  
-              </label>
+                <div>List of questions that will be voted on.</div>
+                <VariableLengthList contentList = {questionTexts} 
+                    onChange = {this.props.onChangeQuestion}
+                    onRemove = {this.props.onRemoveQuestion}
+                    onAdd = {this.props.onAddQuestion}
+                    subLists = {optionLists}/> 
             </div>);
   }
 }
@@ -109,13 +193,93 @@ class TextForm extends React.Component {
 
 class VoterList extends React.Component {
   render() {
-    return null;
+    return ( <div>
+                <div>List of voters.</div>
+                <VariableLengthList contentList = {this.props.voterList} 
+                    onChange = {this.props.onChangeVoter}
+                    onRemove = {this.props.onRemoveVoter}
+                    onAdd = {this.props.onAddVoter}/> 
+            </div>);
+  }
+}
+
+
+class Ballot extends React.Component {
+  render() {
+    return ( <div>
+            <div>Please type in your allocated vote tokens in the appropriate boxes.</div>
+            {this.props.questions.map((question, Counter) => {
+              return <div>
+                <p>{question.text}</p>
+                <VariableLengthList contentList = {this.props.votes[Counter]}
+                                    labels = {question.options.map((optionText, i) => {
+                                      return optionText + "; Votes purchased: " + String(voteTokensToVotes(this.props.votes[Counter][i])) + " ";
+                                    })} 
+                                    onChange = {(optionNumber, e) => {this.props.onChangeVote(Counter, optionNumber, e)}}
+                                    inputType = "number"
+                                    />
+            </div>})}
+        </div>);
+  }
+}
+
+
+class VoteResults extends React.Component {
+  render() {
+    return ( <div>
+            {this.props.questions.map((question, Counter) => {
+              return <div>
+                <p>{question.text}</p>
+                <ol>
+                  {question.options.map((option, optionNumber) => {
+                    return <li>{option + "; Votes: " + String(this.props.voteTotals[Counter][optionNumber])}</li>;
+                  })}
+                </ol>
+            </div>})}
+        </div>);
+  }
+}
+
+
+class VariableLengthList extends React.Component {
+  render () {
+    return (
+      <div>
+        <ol>
+          {this.props.contentList.map((text, Counter) => {
+              return <li key = {Counter}>
+                      <TextForm formText = {text} 
+                                label = {this.props.labels ? this.props.labels[Counter] : null}
+                                onChange = {(e) => this.props.onChange(Counter, e)}
+                                onRemove = {this.props.onRemove ? () => {this.props.onRemove(Counter)} : null}
+                                inputType = {this.props.inputType}/>
+                      {this.props.subLists ? this.props.subLists[Counter] : null}
+                    </li>
+          })}
+        </ol>
+        {this.props.onAdd ? <button onClick={this.props.onAdd}>+</button> : null}
+      </div>);
+  }
+}
+
+
+class TextForm extends React.Component {
+  render() {
+    return ( <div>
+              {this.props.label ? this.props.label : null}
+              <label>
+                <input type = {this.props.inputType ? this.props.inputType : "text"} 
+                        value={this.props.formText} 
+                        onChange={this.props.onChange} />      
+                {this.props.onRemove ? <button onClick = {this.props.onRemove}>X</button> : null}  
+              </label>
+            </div>);
   }
 }
 
 class VotingBudget extends React.Component {
   render() {
-    return (<div><TextForm label = "Vote budget:" text = {this.props.voteBudget} onEdit = {this.props.onEdit}/></div>);
+    return (<TextForm label = "Vote budget:" formText = {this.props.voteBudget} onChange = {this.props.onChange} type = "number"/>);
   }
 }
 
